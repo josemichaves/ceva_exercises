f391b4871dbeb474e1dabb801365edd1cddf8d83
Josemi Chaves <josemichaves@gmail.com>
2023-11-11 00:37:24 +0100

    added mongoDb folder, and created seeder

---
+++ mongodb/ex8/index.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const result = await db.collections('users').find(...);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
