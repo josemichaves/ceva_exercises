7f95876831f47cff389552b0f307bf92283faa61
Josemi Chaves <josemichaves@gmail.com>
2023-11-10 18:46:14 +0100

    added node exercises

---
+++ node/ex1/index.js
@@ -0,0 +1,13 @@
+import got from 'got'
+
+// Call web service and return count user, (got is library to call url)
+async function getCountUsers() {
+	return {
+		total: await got.get('https://my-webservice.moveecar.com/users/count').json()
+	};
+}
+// Add total from service with 20
+async function computeResult() {
+	const result = await getCountUsers();
+	return result.total + 20;
+}
