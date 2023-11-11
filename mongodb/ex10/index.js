41c53eed0733019333d718aad3bf42ff1fed4cfe
Josemi Chaves <josemichaves@gmail.com>
2023-11-11 01:02:55 +0100

    added ex10 update and push

---
+++ mongodb/ex10/index.js
@@ -0,0 +1,56 @@
+require("dotenv").config()
+const {
+    MongoClient,
+    ServerApiVersion
+} = require('mongodb');
+const {
+    seedDb
+} = require("./seeders/usersSeeder");
+
+const uri = process.env.MONGODB_URI
+
+// Create client
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+    await seedDb().catch(console.dir);
+
+    const _id = "5cd96d3ed5d3e20029627d4a"
+
+    try {
+        // Connect to db
+        await client.connect();
+
+        // Select collection
+        const collection = client.db('ex10').collection('users');
+
+        // Update date, address and push new role into document
+        await collection.updateOne({
+            _id
+        }, {
+            $set: {
+                last_connection_date: new Date(),
+                addresses: {
+                    zip: 75001,
+                    city: "Paris 1"
+                }
+            },
+            $push: {
+                roles: "admin"
+            }
+        })
+
+    } finally {
+        await client.close()
+        console.log(`Document with _id ${_id} updated succesfully`)
+    }
+}
+// Run the code
+run().catch(console.dir)
---
+++ mongodb/ex10/seeders/usersSeeder.js
@@ -0,0 +1,59 @@
+require("dotenv").config()
+const faker = require("faker");
+const {
+    MongoClient,
+    ServerApiVersion
+} = require('mongodb');
+
+
+const uri = process.env.MONGODB_URI
+
+// Create client
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+module.exports.seedDb = async function seedDb() {
+
+    try {
+        // Connect to db
+        await client.connect();
+
+        // Select collection
+        const collection = client.db('ex10').collection('users');
+
+        // Drop existing collection to have clean data to work
+        await collection.drop();
+
+        // Initialize empty array to store roles
+        let roles = [];
+
+        for (let i = 0; i < 2; i++) {
+            roles.push(faker.lorem.word())
+        }
+
+        // Insert document, with specified ID
+        await collection.insertOne({
+            _id: "5cd96d3ed5d3e20029627d4a",
+            email: faker.internet.email(),
+            first_name: faker.name.firstName(),
+            last_name: faker.name.firstName(),
+            roles: roles,
+            last_connection_date: faker.date.recent(),
+            addresses: {
+                zip: faker.address.zipCode,
+                city: faker.address.cityName
+            }
+        })
+    } finally {
+
+        await client.close()
+
+        console.log('Seeding complete :)')
+    }
+}
+
