87ecf5c2efbd01f6c587045dee3d8ba8a84bc576
Josemi Chaves <josemichaves@gmail.com>
2023-11-12 17:21:07 +0100

    ex6

--- angular/ex6/src/app/app-user-form/app-user-form.component.ts
+++ angular/ex6/src/app/app-user-form/app-user-form.component.ts
@@ -1,17 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-app-user-form',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      app-user-form works!
-    </p>
-  `,
-  styleUrl: './app-user-form.component.css'
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './app-user-form.component.html',
 })
 export class AppUserFormComponent {
+  userForm: FormGroup;
 
+  @Output()
+
+  // Type for event
+  event = new EventEmitter<{
+    email: string;
+    name: string;
+    birthday: Date;
+    address: { zip: number; city: string };
+  }>();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required, Validators.maxLength(128)]],
+      birthday: [null, [this.dateValidator]], // Call custom validator
+      address: this.formBuilder.group({
+        zip: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$')]], // Regex to only accept only alpha uppercase and lower and space
+        city: ['', [Validators.required]],
+      }),
+    });
+  }
+
+  doSubmit(): void {
+    // check if date is valid for emitting
+    if (this.userForm.valid) {
+      console.log('valid, emitting');
+      this.event.emit({
+        email: this.userForm.get('email')?.value,
+        name: this.userForm.get('name')?.value,
+        birthday: this.userForm.get('birthday')?.value,
+        address: {
+          zip: this.userForm.get('address.zip')?.value,
+          city: this.userForm.get('address.city')?.value,
+        },
+      });
+    }
+  }
+
+  dateValidator(control: FormControl): { isValidDate: boolean } {
+    return {
+      isValidDate:
+        Date.parse(new Date().toDateString()) > Date.parse(control.value), // Parse both dates to timestamp, if provided is lower than today, means is valid date
+    };
+  }
 }
--- angular/ex6/src/app/app.component.ts
+++ angular/ex6/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AppUserFormComponent } from './app-user-form/app-user-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [CommonModule, RouterOutlet, AppUserFormComponent],
+  template: ` <app-app-user-form> </app-app-user-form> `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ex6';
