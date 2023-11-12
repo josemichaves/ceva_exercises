4d85d2a92806475c4bdb73117adbe50c02e01828
Josemi Chaves <josemichaves@gmail.com>
2023-11-12 15:33:25 +0100

    initialised angular ex6

---
+++ angular/ex6/src/app/app-user-form/app-user-form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-user-form',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      app-user-form works!
+    </p>
+  `,
+  styleUrl: './app-user-form.component.css'
+})
+export class AppUserFormComponent {
+
+}
